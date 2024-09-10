4d1003dbc0e20655bd0c10cfb9c88cfbe5e7a394
Anay Mehra <anaymehra63@gmail.com>
2024-09-10 20:27:23 +0530

    2

--- server/db.js
+++ server/db.js
@@ -1,11 +1,7 @@
 import pg from "pg"
 
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    password: "1234567890",
-    database: "perntodo",
-    port: 5432
+    connectionString: process.env.POSTGRES_URL,
 });
 
 export default db;
